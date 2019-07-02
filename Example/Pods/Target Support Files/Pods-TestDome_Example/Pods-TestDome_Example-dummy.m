92d183dd72b92b31bba6daf96383c91e9c2b8574
liuliang <454493675@qq.com>
2019-07-02 09:30:19 +0800

    初始化

---
+++ Example/Pods/Target Support Files/Pods-TestDome_Example/Pods-TestDome_Example-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_TestDome_Example : NSObject
+@end
+@implementation PodsDummy_Pods_TestDome_Example
+@end
---
+++ Example/Pods/Target Support Files/TestDome/TestDome-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_TestDome : NSObject
+@end
+@implementation PodsDummy_TestDome
+@end
